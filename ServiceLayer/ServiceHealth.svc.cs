eabeaf3e43d9a80d6c1a007a3abf3aa0a14b8d69
gonga <gonga@DESKTOP-KISGEL3>
2017-03-18 23:11:29 +0000

    x

--- ServiceLayer/ServiceHealth.svc.cs
+++ ServiceLayer/ServiceHealth.svc.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
-using System.Data.Entity.Migrations;
 using System.Data.Entity.Validation;
 using System.Linq;
 
